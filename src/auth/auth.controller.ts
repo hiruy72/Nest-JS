222ba8708ab885489c04dbc045424bb2c7cd1283
Hiruy Legesse <hiruyadane@gmail.com>
2025-12-17 23:45:18 -0800

    Add login controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from 'src/posts/dto/regiser-dto';
+import { LoginDto } from 'src/posts/dto/login-dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
     @Post('register')
     register(@Body()registerDto: RegisterDto){
         return this.authService.regigisteruser(registerDto);
+    }
+    @Post('login')
+    login(@Body() loginDto: LoginDto){
+        return this.authService.LoginUser(loginDto);
+
     }
 }
