1165871e275cf3874b6bcf13844e35d332493335
Hiruy Legesse <hiruyadane@gmail.com>
2025-12-22 01:13:19 -0800

    add role validator method

--- src/auth/decorators/roles.decoraters.ts
+++ src/auth/decorators/roles.decoraters.ts
@@ -4,4 +4,4 @@ import { UserRole } from "../entities/user-entity";
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);  // set user role metadata
--- src/auth/guards/roles-guard.ts
+++ src/auth/guards/roles-guard.ts
@@ -1,13 +1,40 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
+import { UserRole } from "../entities/user-entity";
+import { ROLES_KEY } from "../decorators/roles.decoraters";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private reflector: Reflector) {}
+    constructor(private reflector: Reflector) {} // help us access the metadata
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    //like ->  next method in the middleware
+    canActivate(context: ExecutionContext): boolean{
+        const requireRole = this.reflector.getAllAndOverride<UserRole[]>(
+            ROLES_KEY, [
+                context.getHandler(),
+                context.getClass(),
+            ]
+        )
+        if(!requireRole){
+            return true
+        }
+
+        const {user} = context.switchToHttp().getRequest()
+
+        if(!user){
+            throw new ForbiddenException('not authenticated')
+        }
+
+        const hasRequiredRole = requireRole.some(role=> user.role===role)
+
+        if(!hasRequiredRole){
+            throw new ForbiddenException('not allowed')
+        }
+
+        return true
+
         
     }
 
-}
+} 
